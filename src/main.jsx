7e79322a4d97f95f8ec3d071cec795e331db0324
ALSK <alsk@users.noreply.github.com>
2025-12-05 14:09:53 +0000

    Add fallback Privy App ID

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { PrivyProvider } from '@privy-io/react-auth'
 import { AuthProvider } from './context/AuthContext'
 import App from './App.jsx'
 
-const PRIVY_APP_ID = import.meta.env.VITE_PRIVY_APP_ID || '';
+const PRIVY_APP_ID = import.meta.env.VITE_PRIVY_APP_ID || 'cmisw6p5r00s5k00ct100zok3';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
